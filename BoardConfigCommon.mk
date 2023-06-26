b3895b2040e05907c39de5ddfd1d08a4f6784371
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-26 16:09:23 +0800

    sm8350-common: Switch left overs to common components
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,9 +45,6 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY ?= 440
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
-
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
@@ -58,7 +55,7 @@ DEVICE_MATRIX_FILE += $(COMMON_PATH)/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
